bc2a130cdf0a08c984aeea6c6e93ad27c6866b91
Alejandro Gutierrez <lovexknife@outlook.com>
2020-12-28 16:29:22 -0600

    practice finished

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
 const promise = welcome();
+promise.then((result)=>{
+    console.log(`The result is: ${result}`);
+})
 
-console.log(promise);
+const question = `Will the weather be nice today?`;
+const answer = tell();
+answer.then((val)=>{
+    console.log(question)
+    console.log(val);
+}).catch(console.log);  //val is the value that's being returned from answer, which is the tell function 
+//with the question parameter
+
+console.log(promise); //the terminal will display this first, then do all the async queue
